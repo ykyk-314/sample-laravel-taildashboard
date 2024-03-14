21af291c9bb21b8f274161a7ac1f9f1d57f7fa29
genepa-yokoyama <takahiro.yokoyama@genepa.com>
2024-03-14 13:26:39 +0900

    整理

--- resources/views/admin/_layouts/sidebar.blade.php
+++ resources/views/admin/_layouts/sidebar.blade.php
@@ -1,3 +1,5 @@
+<div x-cloak :class="sidebarOpen ? 'block' : 'hidden'" @click="sidebarOpen = false" class="fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden"></div>
+
 <div x-cloak :class="sidebarOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
     class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-900 lg:translate-x-0 lg:static lg:inset-0">
     <div class="flex items-center justify-center mt-8">
